0fd19863302ca9a0c967142d7b3079d156c92caf
Gabriel Assis <tiburciogabriel95@gmail.com>
2020-09-30 23:04:25 -0300

    PilhaDinamica

---
+++ PilhaDinamica/Conversor.java
@@ -0,0 +1,22 @@
+public class Conversor {
+	public static void main(String args[]) {
+		int numero = Integer.parseInt(args[0]);
+		PilhaDinamica p = new PilhaDinamica();
+		// fase 1 = empilhamento dos restos
+		int resto;
+		while(numero != 0) {
+			resto = numero % 2; //pego o resto da divisao desse número por 2 (vale 0 ou 1)
+			p.push(resto); //armazeno na pilha
+			numero = numero /2; //gero um número,resultado da divisão por 2
+		}
+		// fase 2 = exibicao dos restos
+		while(!p.isEmpty()){
+			resto = p.pop(); //remove da pilha (Sempre o cara que está no tpo)
+			System.out.print(resto);
+		}
+		System.out.println("\n fim do programa");
+		
+	}
+
+}
+	
